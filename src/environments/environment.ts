7a4fee55e75d49bc69fe73bd50d6b822dfa95819
Magno Sescon <magnosescon@gmail.com>
2020-01-07 08:18:32 -0300

    First Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAtfOKG8ZegVR5troWqYeDdlQCJNXKqio4",
+    authDomain: "quiz-fighter-93313.firebaseapp.com",
+    databaseURL: "https://quiz-fighter-93313.firebaseio.com",
+    projectId: "quiz-fighter-93313",
+    storageBucket: "quiz-fighter-93313.appspot.com",
+    messagingSenderId: "644662157139",
+    appId: "1:644662157139:web:c1faf73ba278a168d70ba6",
+    measurementId: "G-WJ5TZ3NV3V"
+  }
 };
 
 /*
