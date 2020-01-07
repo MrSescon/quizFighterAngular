21d2807d6481c78146a3599ba914e842148e49e4
Magno Sescon <magnosescon@gmail.com>
2020-01-07 08:42:19 -0300

    instalado e configurado os modulos do firebase e angularfire2; importados os assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAtfOKG8ZegVR5troWqYeDdlQCJNXKqio4",
+    authDomain: "quiz-fighter-93313.firebaseapp.com",
+    databaseURL: "https://quiz-fighter-93313.firebaseio.com",
+    projectId: "quiz-fighter-93313",
+    storageBucket: "quiz-fighter-93313.appspot.com",
+    messagingSenderId: "644662157139",
+    appId: "1:644662157139:web:c1faf73ba278a168d70ba6",
+    measurementId: "G-WJ5TZ3NV3V"
+  }
 };
