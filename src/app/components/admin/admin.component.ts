769597f6ce559916f116f0395fcda2196f31ec63
Magno Sescon <magnosescon@gmail.com>
2020-02-19 05:41:53 -0300

    Implementado o css da tela de administração; Implementado o HTML da tela de administração; Implementado ações de logout, autorização, e listagem de perguntas.

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
+import {
+  MatTableDataSource, MatPaginator, 
+  MatDialog, 
+  MatDialogRef
+} from '@angular/material';
+import { environment as env } from '../../../environments/environment';
+
+import {PerguntasService } from '../../services';
+import { Pergunta } from '../../models';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +18,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  colunas = ['pergunta', 'opcoes', 'correta', 'acao'];
+  dataSource: MatTableDataSource<Pergunta>;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  
+  constructor(
+    private angularFireAuth: AngularFireAuth,
+    private router: Router,
+    private dialog: MatDialog,
+    private perguntasService: PerguntasService
+  ) { }
 
   ngOnInit() {
+    this.validarAuntenticacao();
+    this.perguntasService.obterPerguntas()
+      .subscribe(perguntas => {
+        this.dataSource = new MatTableDataSource<Pergunta>(perguntas);
+        this.dataSource.paginator = this.paginator;
+      });
+  }
+
+  validarAuntenticacao() {
+    this.angularFireAuth.authState.subscribe(authState => {
+      if(!authState || authState.email != env.adminEmail) {
+        this.router.navigate(['/']);
+      }
+    });
+  }
+
+  sair() {
+    this.angularFireAuth.auth.signOut();
   }
 
 }
